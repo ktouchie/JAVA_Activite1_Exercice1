8a79b69c38663d70da47f88e1e9f2ac1cecc5154
Karin Touchie <ktouchie@gmail.com>
2015-12-08 13:10:17 +0100

    cm² change to centimetres squared

--- JAVA_Exercice1/Exercice1.java
+++ JAVA_Exercice1/Exercice1.java
@@ -3,11 +3,11 @@ public class main{
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the base of the triangle (cm):");
+        System.out.println("Enter the base of the triangle in centimetres:");
         int base = sc.nextInt();
-        System.out.println("Enter the height of the triangle (cm):");
+        System.out.println("Enter the height of the triangle in centimetres:");
         int height = sc.nextInt();
         int area = (base*height)/2
-        System.out.println("The area of the triangle is" + area"cm²")
+        System.out.println("The area of the triangle is " + area" centimetres squared")
     }
 }
