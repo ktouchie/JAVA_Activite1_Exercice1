828faa981246fa92cf017f8249e3602e9b428100
Karin Touchie <ktouchie@gmail.com>
2015-12-08 13:32:43 +0100

    Change class again

--- JAVA_Exercice1/Exercice1.java
+++ JAVA_Exercice1/Exercice1.java
@@ -3,11 +3,11 @@ public class main{
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the base of the triangle in centimetres:");
+        System.out.println("Enter the base of the triangle:");
         int base = sc.nextInt();
-        System.out.println("Enter the height of the triangle in centimetres:");
+        System.out.println("Enter the height of the triangle:");
         int height = sc.nextInt();
-        int area = (base*height)/2
-        System.out.println("The area of the triangle is " + area" centimetres squared")
+        int area = ((base*height)/2);
+        System.out.println("The area of the triangle is " + area)
     }
 }
