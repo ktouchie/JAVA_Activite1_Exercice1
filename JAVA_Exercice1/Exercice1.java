e7d1db5cb56838dc196048533a5388af7efa673f
Karin Touchie <ktouchie@gmail.com>
2015-12-08 12:55:58 +0100

    Exercice1.java

---
+++ JAVA_Exercice1/Exercice1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the base of the triangle (cm):");
+    int base = sc.nextInt();
+    System.out.println("Enter the height of the triangle (cm):");
+    int height = sc.nextInt();
+    int area = (base*height)/2
+    System.out.println("The area of the triangle is" + area"cm²")
+}
